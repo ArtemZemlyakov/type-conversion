1ddcbb9ee99de986edd30caa67426d84ab6ffcb0
ArtemZemlyakov <139268188+ArtemZemlyakov@users.noreply.github.com>
2023-07-15 11:00:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let age = 18
+console.log(String(age), Number(age), Boolean(age));
+let name = 'Artem';
+console.log(String(name), Number(name), Boolean(name));
+let gender = 'M';
+console.log(String(gender), Number(gender), Boolean(gender));
+let whoAmIInProgramming = 'student';
+console.log(String(whoAmIInProgramming), Number(whoAmIInProgramming), Boolean(whoAmIInProgramming));
+let experienceInJavaScript = 0;
+console.log(String(experienceInJavaScript), Number(experienceInJavaScript), Boolean(experienceInJavaScript));
+let training = 'hard';
+console.log(String(training), Number(training), Boolean(training));
+let programmingLanguage = 'JS';
+console.log(String(programmingLanguage), Number(programmingLanguage), Boolean(programmingLanguage));
+let hello = 'Hello World';
+console.log(String(hello), Number(hello), Boolean(hello));
